07e43d1b36a578e06242a55c0f2c2060b2cef7e9
vignastars <60565963+vignastars@users.noreply.github.com>
2020-02-14 15:48:32 +0530

    Add files via upload

---
+++ matrixadd.py
@@ -0,0 +1,18 @@
+n=int(input("enter the size"))
+m=int(input("enter the size"))
+for i in range(n):
+    for j in range(m):
+        x = int(input("enter the numbers:"))
+        a.append(x)
+        a = []
+    for i in range(n):
+    for j in range(m):
+        x = int(input("enter the numbers:"))
+        b.append(y)
+        b = []
+        
+    for i in range(n):
+        for j in range(m):
+            print(a[i][j]+b[i][j],end="")
+            print(end=" \n ")
+        
